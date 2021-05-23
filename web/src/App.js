19a18350ff021e67e54e30657d3ba3661482ccc0
Dylan Russo <dylan.a.russo@gmail.com>
2021-05-23 13:44:22 -0400

    Update css

--- web/src/App.js
+++ web/src/App.js
@@ -102,19 +102,20 @@ const fetchAnswers = async (id) => {
 
     <>
 
+
     <Modal title="New Question" visible={showNewQuestion} onOk={onCreateQuestion} onCancel={() => {setShowNewQuestion(false)}}>
         <textarea value={newQuestionText} onChange={(event) => {
             setNewQuestionText(event.currentTarget.value)
         }} placeholder={'Enter your new question'} className={'w-full border p-2'}></textarea>
     </Modal>
 
-    <div className={'grid grid-cols-12'}>
-        <div className={'col-span-12 border p-8 bg-gray-200'}>
-            <h1 className={'text-center text-2xl'}>App Title</h1>
+    <div className={'grid grid-cols-12 font-serif text-white'}>
+        <div className={'col-span-12 border p-8 bg-gray-300'}>
+            <h1 className={'text-center text-2xl text-red-600'}>Question & Answer Forum</h1>
 
         </div>
 
-        <div className={'col-span-12 sm:col-span-4 md:col-span-3 border'}>
+        <div className={'col-span-12 sm:col-span-4 md:col-span-3 border bg-red-600'}>
 
 
             <ul>
@@ -123,29 +124,34 @@ const fetchAnswers = async (id) => {
                                onClick={() => {
                                    setSelectedCategory(category.id)
                                }}
-                               className={selectedCategory==category.id ? 'bg-gray-400 cursor-pointer p-12 border-b font-bold text-xl text-center' : 'cursor-pointer p-12 border-b font-bold text-xl text-center'}>{category.name}</li>
+                               className={selectedCategory==category.id ? 'bg-green-500 cursor-pointer p-12 border-b font-bold text-xl text-center' : 'cursor-pointer p-12 border-b font-bold text-xl text-center'}>{category.name}</li>
                 })}
             </ul>
 
         </div>
 
-        <div className={'col-span-12 sm:col-span-8 md:col-span-9 p-10'}>
-            {!selectedCategory && <h1 className={'text-center text-2xl'}>Select a category to view the questions</h1>}
+
+        <div className={'col-span-12 sm:col-span-8 md:col-span-9 p-10 bg-green-500 '}>
+            {!selectedCategory && <h1 className={'text-center text-3xl text-white'}>Select a category to view the questions</h1>}
             {selectedCategory && <button onClick={() => {setShowNewQuestion(true)}} className={'text-white pr-4 pl-4 pt-3 pb-3 bg-blue-500 rounded cursor-pointer'}>New Question</button>}
+          
+          <div className={'text-center'}>
 
             <p>Once a user clicks on a new question button above, user should be able to see the new questions form here</p>
             <p>Once the user enters the information in the form and hits submit, you should fetch the questions for the category</p>
 
+          </div>
+
             {selectedCategory && <Collapse onChange={onChange} accordion>
                 {questions && questions.map((question) => {
                     return <Panel header={question.questionTxt} key={question.id}>
-                        <input className={'border p-2 mb-2 w-1/3 block'} type="text" placeholder={'Start typing your answer for this question'} value={answerTxt} onChange={(event) => {
+                        <input className={'border p-2 mb-2 w-1/3 block bg-gray-200'} type="text" placeholder={'Start typing your answer!'} value={answerTxt} onChange={(event) => {
                             setAnswerTxt(event.currentTarget.value)
                         }}/>
 
                         <button className={'text-white pr-3 pl-3 pt-2 pb-2 bg-blue-500 rounded cursor-pointer'} onClick={createAnswer}>Submit</button>
 
-                        <p>List out all the answers for a question</p>
+                        <p>The Intelligent Answers</p>
                         <ul>
                             {answers && answers.map((answer) => {
                                 return <li>{answer.answerTxt}</li>
